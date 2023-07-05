d801ac9e68df4e5ff18ee1e84dce7739a7657c25
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-07-05 12:14:25 +0100

    Bare minimum

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "Welcome to my app"
+	})
+})
+
+app.listen(process.env.PORT, () => {
+	console.log("Listening on port,", process.env.PORT)
+})
