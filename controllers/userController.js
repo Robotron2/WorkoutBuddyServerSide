94e9b8fab33a2a3f57d91ce05a578976dfbf830a
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-08-16 13:34:51 +0100

    Generated  jwt_token on signUp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
 const validator = require("validator")
+const jwt = require("jsonwebtoken")
+
+const createToken = (_id) => {
+	return jwt.sign({ _id }, process.env.JWTSECRET, { expiresIn: "3d" })
+}
 //login user
 const loginUser = async (req, res) => {
 	//
@@ -10,6 +15,7 @@ const loginUser = async (req, res) => {
 //signup user
 const signupUser = async (req, res) => {
 	const { email, password } = req.body
+
 	try {
 		//validation
 		if (!email || !password) {
@@ -37,7 +43,10 @@ const signupUser = async (req, res) => {
 			password: hash
 		})
 
-		res.status(200).json({ email, user })
+		//create token
+		const token = createToken(user._id)
+
+		res.status(200).json({ email, token })
 	} catch (error) {
 		res.status(400).json({ error: error.message })
 	}
